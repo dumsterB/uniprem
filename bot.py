2c99f5029847cd9c37b6e2d6b868a1795a464352
Dumster <abduvalievbehr.uz1@gmail.com>
2022-12-17 00:24:01 +0500

    initiall commit

---
+++ bot.py
@@ -0,0 +1,26 @@
+import os
+
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+TOKEN='5879394130:AAGw9NgBEpycnISeNDfUP5VF_sYjf11UUYQ'
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo_send(message: types.Message):
+    link = message.text
+    splitted = [*link]
+    arr = []
+    for i in range(len(splitted)):
+        if (splitted[i] == '_'):
+            arr.append([splitted[i + 1], splitted[i + 2], splitted[i + 3], splitted[i + 4], splitted[i + 5], splitted[i + 6],splitted[i + 7], splitted[i + 8]])
+    my_lst_str = ''.join(map(str, arr[0]))
+    res = f'https://www.freepik.com/download-file/{my_lst_str}'
+    await message.reply(res)
+
+
+executor.start_polling(dp, skip_updates=True)
---
+++ bot_run.bat
@@ -0,0 +1,11 @@
+@echo off
+
+call %~dp0venv\Scripts\activate
+
+cd%~dp0
+
+set TOKEN=5879394130:AAGw9NgBEpycnISeNDfUP5VF_sYjf11UUYQ
+
+python bot.py
+
+pause
